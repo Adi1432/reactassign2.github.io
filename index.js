1d1a16f1afced9ecbd51ed6a6cf9d4087ef26fd1
Adi1432 <69716922+Adi1432@users.noreply.github.com>
2022-11-19 11:33:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const page = (
+  <div className="main">
+    <h1>
+      The evolution of <br /> Airbins Frontend
+    </h1>
+    <span>@spikebrehm</span>
+    <span>airbnb</span>
+  </div>
+);
+
+ReactDOM.render(page, document.getElementById("root"));
